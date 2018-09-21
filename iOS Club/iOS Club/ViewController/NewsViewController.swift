484323f807bf491dd1d3b69136269461aac44baf
Yang LI <zjzsliyang@gmail.com>
2018-09-21 23:49:41 +0800

    add searchbar

--- iOS Club/iOS Club/ViewController/NewsViewController.swift
+++ iOS Club/iOS Club/ViewController/NewsViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class NewsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupHideKeyboardOnTap()
     }
 
+}
+
+extension NewsViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
+    
+}
+
+extension UIViewController {
+    func setupHideKeyboardOnTap() {
+        self.view.addGestureRecognizer(self.endEditingRecognizer())
+        self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
+    }
+    
+    private func endEditingRecognizer() -> UIGestureRecognizer {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        return tap
+    }
 }
