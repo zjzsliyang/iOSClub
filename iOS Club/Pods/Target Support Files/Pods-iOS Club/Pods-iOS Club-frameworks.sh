5b0a1370c512058eedcdd3ffe96b4d99e60cbc9c
Yang LI <zjzsliyang@gmail.com>
2018-09-22 01:36:43 +0800

    add BMPlayer

--- iOS Club/Pods/Target Support Files/Pods-iOS Club/Pods-iOS Club-frameworks.sh
+++ iOS Club/Pods/Target Support Files/Pods-iOS Club/Pods-iOS Club-frameworks.sh
@@ -144,9 +144,17 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BMPlayer/BMPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkeletonView/SkeletonView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/BMPlayer/BMPlayer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NVActivityIndicatorView/NVActivityIndicatorView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkeletonView/SkeletonView.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ iOS Club/iOS Club/View/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  iOS Club
+//
+//  Created by Yang Li on 2018/9/22.
+//  Copyright © 2018 Yang LI. All rights reserved.
+//
+
+import UIKit
+import BMPlayer
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
