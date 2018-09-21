8f58ce699035396e1ece9ee09c5c3dc614fc40d1
Yang LI <zjzsliyang@gmail.com>
2018-09-21 23:13:57 +0800

    add config

---
+++ iOS Club/iOS Club/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  iOS Club
+//
+//  Created by Yang Li on 2018/9/21.
+//  Copyright © 2018 Yang LI. All rights reserved.
+//
+
+import Foundation
+
+let backendUrl = ""
+let backendPort = ""
+
---
+++ iOS Club/iOS Club/Model/BlogData.swift
@@ -0,0 +1,9 @@
+//
+//  BlogData.swift
+//  iOS Club
+//
+//  Created by Yang Li on 2018/9/21.
+//  Copyright © 2018 Yang LI. All rights reserved.
+//
+
+import Foundation
