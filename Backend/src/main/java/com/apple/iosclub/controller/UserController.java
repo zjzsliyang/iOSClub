b9a8aa9b0d3631ea70b27697589066605b1634a4
lzj <tjluozhongjin@gmail.com>
2018-09-22 18:02:56 +0800

    登陆注册，文章获取

--- Backend/src/main/java/com/apple/iosclub/controller/UserController.java
+++ Backend/src/main/java/com/apple/iosclub/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.apple.iosclub.controller;
 
-import com.apple.iosclub.FormatModel.FormatUser;
+import com.apple.iosclub.FormatModel.User;
 import com.apple.iosclub.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.UnknownHostException;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/user")
@@ -15,8 +16,73 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping("/getInfoByEmail")
-    public Object getByEmail(String email){
-        return new FormatUser(userMapper.getByEmail(email));
+    public Object getByEmail(String email) throws UnknownHostException {
+        return new User(userMapper.getByEmail(email));
+    }
+
+    @PostMapping("/login")
+    public Object login(@RequestBody HashMap<String, Object> req){
+
+        String email = (String) req.get("email");
+        String password = (String) req.get("password");
+
+        String real = (String)userMapper.getPassWord(email);
+
+
+
+        HashMap<String, Object> res = new HashMap<>();
+
+        if (real==null){
+            res.put("code", -1);
+            res.put("msg", "用户不存在");
+            return res;
+        }
+
+        if (password.equals(real)){
+            res.put("code", 0);
+            res.put("msg", "登陆成功");
+        }else {
+            res.put("code", 1);
+            res.put("msg", "密码错误");
+        }
+        return res;
+
+    }
+
+    @PostMapping("/register")
+    public Object register(@RequestBody HashMap<String, Object> req){
+        HashMap<String, Object> res = new HashMap<>();
+        try {
+
+            String username =  (String)req.get("username");
+            int u_code = (int) req.get("u_code");
+            int privilege = (int) req.get("user_privilege");
+            String email = (String) req.get("email");
+            String real = (String)userMapper.getPassWord(email);
+            if (real!=null){
+                res.put("code", -1);
+                res.put("msg", "邮箱已经注册");
+                return res;
+            }
+            String password = (String) req.get("password");
+            String position =  (String) req.get("position");
+            String description =  (String) req.get("description");
+            String avatar =  "";
+            userMapper.insertUser(username, u_code, privilege, email, password, position, description, avatar);
+
+            res.put("code", 0);
+            res.put("msg", "注册成功");
+
+        }catch (Exception e){
+            e.printStackTrace();
+            res.put("code", 1);
+            res.put("msg", "注册失败");
+        }
+
+
+        return res;
+
+
     }
 
 }
--- Backend/src/main/java/com/apple/iosclub/mapper/UserMapper.java
+++ Backend/src/main/java/com/apple/iosclub/mapper/UserMapper.java
@@ -1,17 +1,30 @@
 package com.apple.iosclub.mapper;
 
-import com.apple.iosclub.Entity.User;
+import com.apple.iosclub.Entity.MyUser;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface UserMapper {
 
 
     @Select("select * from user natural join university where email=#{email}")
-    User getByEmail(String email);
+    MyUser getByEmail(String email);
 
+    @Select("select password from user where email=#{email}")
+    String getPassWord(String email);
+
+    @Insert({"insert into user(username, u_code, privilege, email, password, position, description, avatar) values(#{username},#{u_code},#{privilege}, #{email}, #{password}, #{position}, #{description}, #{avatar})"})
+    void insertUser(@Param("username") String username,
+                    @Param("u_code") int u_code,
+                    @Param("privilege") int privilege,
+                    @Param("email") String email,
+                    @Param("password") String password,
+                    @Param("position") String position,
+                    @Param("description") String description,
+                    @Param("avatar") String avatar);
 
 
 
