fda43e591f0ae072ffdd6e06f385caa57c97c774
Yang LI <zjzsliyang@gmail.com>
2018-09-22 10:06:57 +0800

    add calendar

---
+++ iOS Club/iOS Club/View/ActivityCell.swift
@@ -0,0 +1,21 @@
+//
+//  ActivityCell.swift
+//  iOS Club
+//
+//  Created by Yang Li on 2018/9/22.
+//  Copyright © 2018 Yang LI. All rights reserved.
+//
+
+import UIKit
+
+class ActivityCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
