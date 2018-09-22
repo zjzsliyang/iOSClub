7b811568c9d210de07a7cdfb2650527f223a4d29
Yang LI <zjzsliyang@gmail.com>
2018-09-22 10:07:13 +0800

    update file

--- iOS Club/iOS Club/View/ActivityCell.swift
+++ iOS Club/iOS Club/View/ActivityCell.swift
@@ -7,15 +7,9 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
-class ActivityCell: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+class ActivityCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
 }
--- iOS Club/iOS Club/ViewController/ActivityViewController.swift
+++ iOS Club/iOS Club/ViewController/ActivityViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ActivityViewController: UIViewController {
+    let formatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension ActivityViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        print("test")
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "ActivityCell", for: indexPath) as! ActivityCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy mm dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        let para = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return para
+    }
 }
