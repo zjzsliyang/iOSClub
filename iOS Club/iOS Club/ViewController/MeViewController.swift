d65e376724f78980bc0db93c4f3479084fd12736
lzj <tjluozhongjin@gmail.com>
2018-09-23 14:37:51 +0800

    个人主页

--- iOS Club/iOS Club/ViewController/MeViewController.swift
+++ iOS Club/iOS Club/ViewController/MeViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
 class MeViewController: UIViewController {
 
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var position: UILabel!
+    
+
+  
+    
+    @IBOutlet weak var email: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var emailAddress = "zhuhongming@tongji.edu.cn"
+        
+        Alamofire.request(backendUrl+"/user/getInfoByEmail?email="+emailAddress).responseJSON { response in
+            debugPrint(response)
+            
+            if let data = response.result.value {
+                let json = JSON(data)
+                self.name.text = json["username"].rawString()
+                self.position.text = json["position"].rawString()
+                self.email.text = json["email"].rawString()
+                
+                let url = URL(string: json["avatar"].rawString()!)
+                URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        if let image = UIImage(data: data!) {
+                            self.avatar.image = image
+                        }
+                    }
+                }).resume()
+                
+            }
+        }
+        
     }
 
 }
