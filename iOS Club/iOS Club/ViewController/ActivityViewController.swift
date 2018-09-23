7af8b268bff6c095a50fe9f729d83532ca35de0a
Yang LI <zjzsliyang@gmail.com>
2018-09-23 17:12:33 +0800

    add activitycell

--- iOS Club/iOS Club/ViewController/ActivityViewController.swift
+++ iOS Club/iOS Club/ViewController/ActivityViewController.swift
@@ -206,3 +206,16 @@ extension ActivityViewController: VACalendarViewDelegate {
     }
     
 }
+
+extension ActivityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "activity")!
+        return cell
+    }
+    
+    
+}
