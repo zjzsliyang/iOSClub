74fcab83f23eb8c19dd0291ea4e965cf03e036c0
Yang LI <zjzsliyang@gmail.com>
2018-09-24 11:47:40 +0800

    fix bugs

--- iOS Club/iOS Club/ViewController/MeViewController.swift
+++ iOS Club/iOS Club/ViewController/MeViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
-import SkeletonView
+import VegaScrollFlowLayout
 
 class MeViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
